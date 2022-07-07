409e4f01942033fd540866f47753d9405c064946
mtgnorton <15726204663@163.com>
2022-07-07 14:35:22 +0800

    修改部分代码以遵循golangci-lint规范

--- app/model/administrator_role.go
+++ app/model/administrator_role.go
@@ -13,5 +13,5 @@ type RoleShow struct {
 	Id             uint   `json:"id"`
 	Name           string `json:"name"`
 	Identification string `json:"identification"`
-	Status         string `json:"status",`
+	Status         string `json:"status"`
 }
